779202c5bac4bd463e339c7a0807e602ae3af3a2
Ewoki-Ebouele-Ange <aewokiebouele@gmail.com>
2023-12-20 13:03:33 +0100

    Mercredi 20

--- pages/accueil.js
+++ pages/accueil.js
@@ -2,56 +2,58 @@ import styles from "./page.module.css"
 import Image from 'next/image'
 import Drop from "../assets/Vector.svg"
 import Sidebar from "./sidebar"
+import { useRouter } from "next/router"
+import Link from "next/link"
 
 export default function Accueil() {
+
+    const router = useRouter()
+
     return (
         <>
-        <Sidebar />
-        <div className={styles.accueil}>
-            <div className={styles.title_slogan}>
-            BIENVENU AU CTD
-            </div>
-            <div className={styles.countries}>
-            <div className={styles.count_itm}>
-                Cameroun
-                <Image src={Drop} alt="Picture" className={styles.drop_link}/>
+            <Sidebar />
+            <div className={styles.accueil}>
+                <div className={styles.title_slogan}>
+                    BIENVENU AU CTD
+                </div>
+                <div className={styles.countries}>
+                    <div className={styles.count_itm}>
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            Cameroun
+                            <Image src={Drop} alt="Picture" className={styles.drop_link} />
+                        </div>
 
-                <ul>
-                <li className={styles.linkin}>
-                    <a href="/regInformations">Littoral</a>
-                </li>
-                <li className={styles.linkin}>
-                    <a href="/regInformations">Centre</a>
-                </li>
-                <li className={styles.linkin}>
-                    <a href="/regInformations">Ouest</a>
-                </li>
-                <li className={styles.linkin}>
-                    <a href="/regInformations">Est</a>
-                </li>
-                <li className={styles.linkin}>
-                    <a href="/regInformations">Sud</a>
-                </li>
-                <li className={styles.linkin}>
-                    <a href="/regInformations">Nord</a>
-                </li>
-                <li className={styles.linkin}>
-                    <a href="/regInformations">Extrême Nord</a>
-                </li>
-                </ul>
-            </div>
+                        <ul>
+                            <li className={styles.linkin}>
+                                <a onClick={() => router.push(`/regInformations?data=${encodeURIComponent("Littoral")}`)}>Littoral</a>
+                            </li>
+                            <li className={styles.linkin}>
+                                <a onClick={() => router.push(`/regInformations?data=${encodeURIComponent("Centre")}`)}>Centre</a>
+                            </li>
+                            <li className={styles.linkin}>
+                                <a onClick={() => router.push(`/regInformations?data=${encodeURIComponent("Ouest")}`)}>Ouest</a>
+                            </li>
+                            <li className={styles.linkin}>
+                                <a onClick={() => router.push(`/regInformations?data=${encodeURIComponent("Est")}`)}>Est</a>
+                            </li>
+                            <li className={styles.linkin}>
+                                <a onClick={() => router.push(`/regInformations?data=${encodeURIComponent("Sud")}`)}>Sud</a>
+                            </li>
+                            <li className={styles.linkin}>
+                                <a onClick={() => router.push(`/regInformations?data=${encodeURIComponent("Nord")}`)}>Nord</a>
+                            </li>
+                            <li className={styles.linkin}>
+                                <a onClick={() => router.push(`/regInformations?data=${encodeURIComponent("Extrême Nord")}`)}>Extrême Nord</a>
+                            </li>
+                        </ul>
+                    </div>
 
-            <div className={styles.infor}>
-                <span> Name : Cameroun</span>
-                <span> Name : Cameroun</span>
-                <span> Name : Cameroun</span>
-                <span> Name : Cameroun</span>
-                <span> Name : Cameroun</span>
-                <span> Name : Cameroun</span>
-            </div>
+                    <div className={styles.informa_reg}>
+                        Afficher les informations sur le Cameroun
+                    </div>
 
+                </div>
             </div>
-        </div>
-      </>
+        </>
     )
 }
--- pages/regInformations.js
+++ pages/regInformations.js
@@ -3,89 +3,69 @@ import Sidebar from "./sidebar"
 import Image from 'next/image'
 import Drop from "../assets/Vector.svg"
 import Drop2 from "../assets/Vector2.svg"
+import { useState } from "react"
+import { useRouter } from "next/router"
 
-export default function Region() {
+ const Region = () =>  {
+
+    const [currentLink, setCurrentLink] = useState();
+    const router = useRouter()
+    const receivedData = router.query.data || 'null';
+
     return (
-     <>
-        <Sidebar />
-        <div className={styles.accueil}>
-            <div className={styles.onglets}>
-                <div className={styles.ong}>ODD</div>
-                <div className={styles.ong}>Secteurs</div>
-                <div className={styles.ong}>Ministère</div>
-                <div className={styles.ong}>Finances</div>
-                <div className={styles.ong}>RH</div>
-            </div>
-            <div className={styles.path}>
-                Cameroun <Image src={Drop2} alt="Picture" className={styles.drop_link} width={8}/> Adamaoua
-            </div>
-            <div className={styles.countries}>
-            <div className={styles.count_itm}>
-                Adamaoua
-                <Image src={Drop} alt="Picture" className={styles.drop_link}/>
-
-                <ul>
-                <li className={styles.linkin}>
-                    <a href="#">Adamaoua dep 1</a>
-                </li>
-                <li className={styles.linkin}>
-                    <a href="#">Adamaoua dep 2</a>
-                </li>
-                <li className={styles.linkin}>
-                    <a href="#">Adamaoua dep 3</a>
-                </li>
-                <li className={styles.linkin}>
-                    <a href="#">Adamaoua dep 4</a>
-                </li>
-                <li className={styles.linkin}>
-                    <a href="#">Adamaoua dep 5</a>
-                </li>
-                <li className={styles.linkin}>
-                    <a href="#">Adamaoua dep 5</a>
-                </li>
-                <li className={styles.linkin}>
-                    <a href="#">Adamaoua dep 6</a>
-                </li>
-                </ul>
-            </div>
-            </div>
-            <div className={styles.countries}>
-            <div className={styles.count_itm}>
-                Littoral
-                <Image src={Drop} alt="Picture" className={styles.drop_link}/>
-
-                <ul>
-                <li className={styles.linkin}>
-                    <a href="#">Littoral dep 1</a>
-                </li>
-                <li className={styles.linkin}>
-                    <a href="#">Littoral dep 2</a>
-                </li>
-                <li className={styles.linkin}>
-                    <a href="#">Littoral dep 3</a>
-                </li>
-                <li className={styles.linkin}>
-                    <a href="#">Littoral dep 4</a>
-                </li>
-                <li className={styles.linkin}>
-                    <a href="#">Littoral dep 5</a>
-                </li>
-                <li className={styles.linkin}>
-                    <a href="#">Adamaoua dep 5</a>
-                </li>
-                <li className={styles.linkin}>
-                    <a href="#">Littoral dep 6</a>
-                </li>
-                </ul>
-            </div>
-            </div>
+        <>
+            <Sidebar />
+            <div className={styles.accueil}>
+                <div className={styles.onglets}>
+                    <div className={styles.ong}>ODD</div>
+                    <div className={styles.ong}>Secteurs</div>
+                    <div className={styles.ong}>Ministère</div>
+                    <div className={styles.ong}>Finances</div>
+                    <div className={styles.ong}>RH</div>
+                </div>
+                <div className={styles.path}>
+                    Cameroun <Image src={Drop2} alt="Picture" className={styles.drop_link} width={8} /> {receivedData}
+                </div>
+                <div className={styles.countries}>
+                    <div className={currentLink === 1 ? styles.count_itm : "none"} onClick={() => { setCurrentLink(1); console.log(currentLink) }}>
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            {receivedData}
+                            <Image src={Drop} alt="Picture" className={styles.drop_link} />
+                        </div>
+                        <ul>
+                            <li className={styles.linkin}>
+                                <a href="#">Adamaoua dep 1</a>
+                            </li>
+                            <li className={styles.linkin}>
+                                <a href="#">Adamaoua dep 2</a>
+                            </li>
+                            <li className={styles.linkin}>
+                                <a href="#">Adamaoua dep 3</a>
+                            </li>
+                            <li className={styles.linkin}>
+                                <a href="#">Adamaoua dep 4</a>
+                            </li>
+                            <li className={styles.linkin}>
+                                <a href="#">Adamaoua dep 5</a>
+                            </li>
+                            <li className={styles.linkin}>
+                                <a href="#">Adamaoua dep 5</a>
+                            </li>
+                            <li className={styles.linkin}>
+                                <a href="#">Adamaoua dep 6</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
 
 
-            <div className={styles.informa_reg}>
-                Afficher les informations sur l'adamaoua
-            </div>
+                <div className={styles.informa_reg}>
+                    Afficher les informations sur {receivedData}
+                </div>
 
-        </div>  
-     </>
+            </div>
+        </>
     )
-  }
+}
+
+export default Region
